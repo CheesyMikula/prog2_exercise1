27f8c888b8bb4aeef29c3c4dc9c2682938cccd6f
Cheesy <71595414+CheesyPlays@users.noreply.github.com>
2021-05-29 14:37:49 +0200

    1.test

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,4 +1,18 @@
 package at.ac.fhcampuswien;
 
 public class App {
+
+    public static boolean checkPassword(String string){
+
+        if(string.length() < 8 || string.length() > 25){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/at/ac/fhampuswien/AppTest.java
+++ src/test/java/at/ac/fhampuswien/AppTest.java
@@ -1,4 +1,28 @@
 package at.ac.fhampuswien;
 
+import at.ac.fhcampuswien.App;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AppTest {
+
+    @BeforeAll
+    static void init(){}
+
+    @BeforeEach
+    void setup(){}
+
+
+    @Test
+    public void pwLength(){
+
+        String string = "abcdefghijk";
+        assertTrue(App.checkPassword(string));
+    }
+
+
 }
